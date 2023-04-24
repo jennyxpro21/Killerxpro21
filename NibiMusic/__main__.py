44399280761d020d3c55446538725fb34c5121c4
ᴍʀ.ɴɪʀᴊᴏɴ <itsnixa7@gmail.com>
2023-04-24 19:57:45 +0600

    Add files via upload

---
+++ NibiMusic/__main__.py
@@ -0,0 +1,17 @@
+from pyrogram import idle
+from pyrogram import Client as Bot
+from NibiMusic.Modules.cache.clientbot import run
+from NibiMusic.config import API_ID, API_HASH, BOT_TOKEN
+
+    
+bot = Bot(
+    ":Nibi:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="NibiMusic/Plugins")
+)
+
+bot.start()
+run()
+idle()
