fb3c9a731a65f9852c466d2714348e0091d60d70
ᴍʀ.ɴɪʀᴊᴏɴ <itsnixa7@gmail.com>
2023-04-24 21:04:44 +0600

    Update misc.py

--- NibiMusic/Plugins/misc.py
+++ NibiMusic/Plugins/misc.py
@@ -42,6 +42,46 @@ async def join_chat(c: Client, m: Message):
         return await user.send_message(chat_id, "» ᴀssɪsᴛᴀɴᴛ ᴀʟʀᴇᴀᴅʏ ᴊᴏɪɴᴇᴅ ᴛʜᴇ ᴄʜᴀᴛ.​")
 
 
+# ×=======================> sᴏɴɢ ᴅᴏᴡɴʟᴏᴀᴅᴇʀ <==================================× #
+
+@Client.on_message(filters.command(["search", "lol"], prefixes=["/", "!"]))
+async def ytsearch(_, message: Message):
+    try:
+        await message.delete()
+    except:
+        pass
+    try:
+        if len(message.command) < 2:
+            return await message.reply_text("» ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ sᴇᴀʀᴄʜ ʙᴀʙʏ !")
+        query = message.text.split(None, 1)[1]
+        m = await message.reply_text("🔎")
+        results = YoutubeSearch(query, max_results=4).to_dict()
+        i = 0
+        text = ""
+        while i < 4:
+            text += f"✨ ᴛɪᴛʟᴇ : {results[i]['title']}\n"
+            text += f"⏱ ᴅᴜʀᴀᴛɪᴏɴ : `{results[i]['duration']}`\n"
+            text += f"👀 ᴠɪᴇᴡs : `{results[i]['views']}`\n"
+            text += f"📣 ᴄʜᴀɴɴᴇʟ : {results[i]['channel']}\n"
+            text += f"🔗 ʟɪɴᴋ : https://youtube.com{results[i]['url_suffix']}\n\n"
+            i += 1
+        key = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="ᴄʟᴏsᴇ",
+                        callback_data=f"forceclose abc|{message.from_user.id}",
+                    ),
+                ]
+            ]
+        )
+        await m.edit_text(
+            text=text,
+            reply_markup=key,
+            disable_web_page_preview=True,
+        )
+    except Exception as e:
+        await message.reply_text(str(e))
 
 # ×=======================> ɪɴғᴏ <==================================× #
 
